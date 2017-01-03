8cdf53a75f8956465d7f353915585dc2a1e847c4
Roppin.L <robin.ljungqvist@gmail.com>
2017-01-03 14:39:13 +0100

    Labb 7.1

---
+++ Labb7/Labb7/basics.js
@@ -0,0 +1,35 @@
+/*
+1 Euro =
+9.53245789 Swedish kronor
+*/
+var app = angular.module('currencyConverter', []);
+app.controller('converter', ['$scope', function ($scope) {
+        $scope.ConvertToEuro = function () {
+            if (typeof ($scope.sek) == "number") {
+                var result = ConvertToEuro($scope.sek);
+                $scope.sekResult = result;
+            }
+            else {
+                $scope.sekResult = "";
+            }
+        };
+        $scope.ConvertToSek = function () {
+            if (typeof ($scope.euro) == "number") {
+                var result = ConvertToSek($scope.euro);
+                $scope.euroResult = result;
+            }
+            else {
+                $scope.euroResult = "";
+            }
+        };
+    }]);
+function ConvertToEuro(input) {
+    var rate = 0.104871;
+    var result = input * rate;
+    return result;
+}
+function ConvertToSek(input) {
+    var rate = 9.53113;
+    var result = input * rate;
+    return result;
+}
---
+++ Labb7/Labb7/basics.ts
@@ -0,0 +1,40 @@
+﻿/*
+1 Euro =
+9.53245789 Swedish kronor
+*/
+
+declare var angular: any;
+
+let app = angular.module('currencyConverter', []);
+
+
+
+app.controller('converter', ['$scope', function ($scope) {
+    $scope.ConvertToEuro = function () {
+        if (typeof($scope.sek) == "number") {
+            var result = ConvertToEuro($scope.sek);
+            $scope.sekResult = result;
+        }
+        else { $scope.sekResult = ""; }
+
+    };
+    $scope.ConvertToSek = function () {
+        if (typeof ($scope.euro) == "number") {
+            var result = ConvertToSek($scope.euro);
+            $scope.euroResult = result;
+        }
+        else { $scope.euroResult = ""; }
+    };
+}]);
+
+function ConvertToEuro(input: number): number {
+    let rate: number = 0.104871;
+    let result: number = input * rate;
+    return result;
+}
+
+function ConvertToSek(input: number): number {
+    let rate: number = 9.53113;
+    let result: number = input * rate;
+    return result;
+}
